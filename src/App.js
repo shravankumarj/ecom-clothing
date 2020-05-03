604bfdacb6d5389abbc3ed821016fb788a0aca45
Shravan Jadigam <shravanjadigam@Shravans-MacBook-Air.local>
2020-05-03 13:55:44 -0400

    added shop and items

--- src/App.js
+++ src/App.js
@@ -3,11 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 import { Route } from 'react-router';
 import Homepage from './pages/homepage.componet'
+import ShopPage from './pages/shop/shop.component'
 
 
 function App() {
   return (
-    <Homepage />
+    <div>
+      <switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </switch>
+    </div>
   );
 }
 

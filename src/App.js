1d67289660354077113971c589ae9a8c236a1697
Shravan Jadigam <shravanjadigam@Shravans-MacBook-Air.local>
2020-05-03 14:47:41 -0400

    added header componet

--- src/App.js
+++ src/App.js
@@ -4,11 +4,12 @@ import './App.css';
 import { Route } from 'react-router';
 import Homepage from './pages/homepage.componet'
 import ShopPage from './pages/shop/shop.component'
-
+import Header from './components/header/header.component'
 
 function App() {
   return (
     <div>
+      <Header />
       <switch>
         <Route exact path='/' component={Homepage} />
         <Route path='/shop' component={ShopPage} />

73250a296f32fe4fbcafeb54a14fa62bf9ac6136
Shravan Jadigam <shravanjadigam@Shravans-MacBook-Air.local>
2020-05-04 20:15:39 -0400

    Added Authentication using username and email

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,6 +1,7 @@
-import firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+
 
 const config = {
     apiKey: "AIzaSyCkrn9obvLL1bPAHwc8187w5VwqYggDKJ8",
@@ -12,15 +13,40 @@ const config = {
     appId: "1:671667034780:web:fc74a745492ab44b626b59",
     measurementId: "G-HX581XKERL"
 }
-
 firebase.initializeApp(config);
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`) //firebase.firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            });
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+}
+
+
 export const auth = firebase.auth();
-export const firstore = firebase.firestore();
+export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
 
-provider.setCustomParameters({ prompt: 'select_accout' })
+provider.setCustomParameters({ prompt: 'select_account' })
 
 export const signInWithGoogle = () => auth.signInWithPopup(provider)
 

e808fb7af200c3be144cdef404e28578178e8097
Shravan Jadigam <shravanjadigam@Shravans-MacBook-Air.local>
2020-05-17 15:47:06 -0400

    Added and refactor the code for categories and other pages

--- src/components/collection-preview/collection-preview.component.js
+++ src/components/collection-preview/collection-preview.component.js
@@ -1,19 +1,20 @@
-import React from 'react'
+import React from 'react';
 
-import './collection-preview.styles.scss'
-import CollectionItem from '../collection-item/collection-item.component'
+import CollectionItem from '../collection-item/collection-item.component';
+
+import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
     <div className='collection-preview'>
-        <div className='title'>
-            {title.toUpperCase()}
-        </div>
+        <h1 className='title'>{title.toUpperCase()}</h1>
         <div className='preview'>
-            {items.filter((items, idx) => idx < 4).map(item => (
-                <CollectionItem key={item.id} item={item} />
-            ))}
+            {items
+                .filter((item, idx) => idx < 4)
+                .map(item => (
+                    <CollectionItem key={item.id} item={item} />
+                ))}
         </div>
-
     </div>
-)
-export default CollectionPreview
+);
+
+export default CollectionPreview;
